ae4ae61ab4a6b1a63ccc655d753161b4c137a1ee
harrytianyz <67923869+harrytianyz@users.noreply.github.com>
2021-01-26 06:00:16 +0800

    Update and rename database-schema.sql. to database-schema.sql

---
+++ database/database-schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE NOC (
+    id SERIAL,
+    NOC text,
+    region text,
+    notes text,
+);
+
+CREATE TABLE athletes (
+    id SERIAL,
+    name text,
+    sex text,
+    height integer,
+    weight integer,
+    team text,
+    (sports?)
+);
+
+CREATE TABLE athletes_NOC_age_competitions (
+    athletes_id integer,
+    NOC_id integer,
+    age_id integer,
+    competitions_id integer,
+);
+
+CREATE TABLE competitions (
+    id SERIAL,
+    year integer,
+    season text,
+    city text,
+);
+
+CREATE TABLE events (
+    id SERIAL,
+    competitions_id integer,
+    sport text,
+    event text,
+    gold integer,
+    silver integer,
+    bronze integer,
+);
+
