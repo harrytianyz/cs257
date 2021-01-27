7570092b6fdfed258a8d8387daf091dea91db2bf
Riaz Kelly <riazkelly@eduroam-180-86.dyn.carleton.edu>
2021-01-26 21:07:06 -0600

    Finished editing the last sql command

--- database/queries.sql
+++ database/queries.sql
@@ -1,12 +1,24 @@
+'''List all the NOCs (National Olympic Committees), in alphabetical order by abbreviation. 
+These entities, by the way, are mostly equivalent to countries. 
+But in some cases, you might find that a portion of a country participated in a particular games 
+(e.g. one guy from Newfoundland in 1904) or some other oddball situation.
+'''
 SELECT countries.noc
 FROM countries
 ORDER BY countries.noc;
 
+'''List the names of all the athletes from Kenya. 
+If your database design allows it, sort the athletes by last name.
+'''
 SELECT DISTINCT athletes.name, countries.noc
 FROM athletes, athletes_countries_age_competitions, countries
 WHERE countries.noc = 'KEN'
 AND athletes.id = athletes_countries_age_competitions.athletes_id
 AND athletes_countries_age_competitions.noc_id = countries.id;
+
+'''List all the medals won by Greg Louganis, sorted by year. 
+Include whatever fields in this output that you think appropriate.
+'''
 
 SELECT events.event, competitions.year
 FROM events, athletes, competitions
@@ -15,10 +27,15 @@ AND athletes.name = 'Gregory Efthimios "Greg" Louganis'
 AND competitions.id = events.competitions_id
 ORDER BY competitions.year;
 
-SELECT COUNT(events.gold), countries.noc
-FROM events, countries, athletes, athletes_countries_age_competitions
-WHERE events.gold = athletes.id
-AND athletes.id = athletes_countries_age_competitions.athletes_id
+'''List all the NOCs and the number of gold medals they have won, 
+in decreasing order of the number of gold medals.
+'''
+
+SELECT COUNT(DISTINCT events.id), countries.noc
+FROM events, countries, athletes_countries_age_competitions
+WHERE events.gold = athletes_countries_age_competitions.athletes_id
 AND athletes_countries_age_competitions.competitions_id = events.competitions_id
+AND athletes_countries_age_competitions.noc_id = countries.id
 GROUP BY countries.noc
-ORDER BY COUNT(events.gold) DESC;
+ORDER BY COUNT(DISTINCT events.id) DESC;
+
